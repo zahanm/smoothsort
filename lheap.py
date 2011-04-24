3dacf7d2d54da1f00339bae3dd272773858ad12f
Zahan Malkani <zahanm@gmail.com>
2011-04-24 05:49:25 -0700

    Added largely blank files to hold implementation

---
+++ lheap.py
@@ -0,0 +1,31 @@
+#
+# -*- coding: utf-8 -*-
+#
+# Zahan Malkani <zahanm at gmail.com>
+#
+# lheap.py
+#
+
+
+class LeonadoHeap:
+  """
+  For eventual usage with the smoothsort algorithm
+  """
+  
+  def __init__(self):
+    """
+    Take any iterable object with orderable elements and adds them to a
+    Leonardo heap
+    """
+    pass
+
+# -- Tests --
+
+def test_lheap_creation():
+  pass
+
+# -- Boilerplate --
+
+if __name__ == '__main__':
+  print('run tests')
+
---
+++ smoothsort.py
@@ -0,0 +1,20 @@
+#
+# -*- coding: utf-8 -*-
+#
+# Zahan Malkani <zahanm at gmail.com>
+#
+# smoothsort.py
+#
+
+# Can't really think of anything to put here until lheaps are implemented
+
+# -- Tests --
+
+def test_smoothsort():
+  pass
+
+# -- Boilerplate --
+
+if __name__ == '__main__':
+  print('run tests')
+
